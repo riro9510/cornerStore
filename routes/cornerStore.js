b694c4f2fdc54e93204e608fa17cc38267c613dd
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-03-19 22:20:41 -0600

    cambios por auth

--- routes/cornerStore.js
+++ routes/cornerStore.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();  
 const {setItem,GeAllItems,editItemById,deleteItemById,createCart,getAllCartItems,editCart,deleteCart,completePurchase} = require('../cornerStore.js');
-
+const passport = require('passport');
+const {isAuthenticated} = require("../middleware/authentificate.js");
 router.get('/items/',GeAllItems);
-router.post("/items/",setItem);
-router.put("/items/:id",editItemById);
-router.delete("/items/:id",deleteItemById);
+router.post("/items/",isAuthenticated,setItem);
+router.put("/items/:id",isAuthenticated,editItemById);
+router.delete("/items/:id",isAuthenticated,deleteItemById);
 
 router.get('/cart/',getAllCartItems);
-router.post("/cart/",createCart);
-router.put("/cart/:id",editCart);
-router.delete("/cart/:id",deleteCart);
-router.put("/cart/CompletePurchase/:id",completePurchase)
+router.post("/cart/",isAuthenticated,createCart);
+router.put("/cart/:id",isAuthenticated,editCart);
+router.delete("/cart/:id",isAuthenticated,deleteCart);
+router.put("/cart/CompletePurchase/:id",isAuthenticated,completePurchase);
+
+router.get("/login",passport.authenticate('github'),(req,res)=>{});
+
+router.get("/logout",function(req,res,next){
+    req.logOut(function(err){
+        if(err){ return next(err);}
+        res.redirect("/cornerStore");
+    })
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const cornerRoutes = require("./routes/cornerStore.js");
 const swaggerRoutes = require('./routes/swagger');
 const app = express();
 const port = process.env.PORT||8080;
-const mongodb = require("./db/connect.js")
+const mongodb = require("./db/connect.js");
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 app.use(cors());
 app.use(express.json());
+app.use(session({
+  secret:"secret",
+  resave: false,
+  saveUninitialized:true,
+}))
+app.use(passport.initialize()).use(passport.session());
 
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin','*');
@@ -20,6 +29,35 @@ app.use(swaggerRoutes);
 app.use('/cornerStore', (req, res, next) => {
   next();  
 }, cornerRoutes);
+
+passport.use(new GitHubStrategy({
+  clientID:process.env.GITHUB_CLIENT_ID,
+  clientSecret:process.env.GITHUB_CLIENT_SECRET,
+  callbackURL:process.env.CALLBACK_URL
+},
+function(accessToken,refreshToken,profile,done){
+  //User.findOrCreate({githubId:profile.id},function(err,user){
+    return done(null,profile);
+  //});
+  
+}
+));
+
+passport.serializeUser((user,done)=>{
+  done(null,user);
+})
+passport.deserializeUser((user,done)=>{
+  done(null,user);
+})
+
+app.get('/cornerStore',(req,res)=>{res.send(req.session.user !== undefined? `logged in as ${req.session.user.displayName}`:"logged Out")});
+app.get('/github/callback',passport.authenticate('github',{
+  failureRedirect: '/api-docs',session:false}),
+  (req,res)=>{
+    req.session.user = req.user;
+    res.redirect('/cornerStore');
+  }
+)
 
 mongodb.initDb((err,mongodb)=>{
   if(err){
